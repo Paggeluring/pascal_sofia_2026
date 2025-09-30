00f683aa71053a92d6379edfb06fb1ee390b3522
Pascal Andersson <anderssonpascal@gmail.com>
2025-09-30 22:27:33 +0200

    Added google form

--- client/src/Components/Hero/Hero.jsx
+++ client/src/Components/Hero/Hero.jsx
@@ -12,10 +12,13 @@ const Hero = () => {
             </div>
             <div className="container px-5 hero-text">
                 <h1 className="hero-title">Vi ska gifta oss...</h1>
-                <p className="hero-subtitle">... och det vill vi såklart fira med er! Vi kommer att viga oss den <b>9:e maj, 2026</b> i Bohuslän.</p>
+                <p className="hero-subtitle">... och det vill vi såklart fira med dig! Vi kommer att viga oss den <b>9:e maj, 2026</b> i Bohuslän.</p>
                 <p>Vigseln kommer att hållas i <b>Hunnebostrands kyrka</b> kl. 13:00 och bröllopsfesten hålls på <b>Ramsviks Stugby & Camping</b></p>
                 <p>Vi älskar era barn, men den här gången älskar vi dem mer på distans! Vi vill att ni föräldrar känner att ni kan släppa loss lite extra och ta en shot med brudparet, utan några förpliktelser!</p>
+                <p>Meddela oss <b>senast 31:a januari</b> om du kommer eller inte. Detta gör du via länken nedan</p>
+                <a href="https://forms.gle/Hw9vQ1FW8tCB6g3F8" target='_blank' className='hero-btn mt-4'>OSA Här</a>
             </div>
+
         </section>
     );
 };
